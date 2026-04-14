fbf992736065121e1f4b7b79641fa41bf37cbc68
ceradev <cesararamissuarez@gmail.com>
2026-04-15 00:02:49 +0100

    chore(observer): add package __init__ for real_time_observer

---
+++ packages/services/real_time_observer/__init__.py
@@ -0,0 +1,9 @@
+from packages.services.real_time_observer.types import (
+    ModelHealthSignal,
+    RealTimeHealthSnapshot,
+)
+
+__all__ = [
+    "ModelHealthSignal",
+    "RealTimeHealthSnapshot",
+]
